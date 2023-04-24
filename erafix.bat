19ecac189acd7b49e8f780e77bcfd3504a2c9dbd
Vedraj Gawas <82878564+gawasvedraj@users.noreply.github.com>
2023-04-24 18:35:55 +0530

    Update erafix.bat
    added directory remover and optimized script

--- erafix.bat
+++ erafix.bat
@@ -3,8 +3,10 @@
 set a=0
 set b=0
 
-taskkill /f /im javaw.exe
-taskkill /f /im erabuddy.exe
+taskkill /f /im javaw.exe > NUL 2>&1
+taskkill /f /im erabuddy.exe > NUL 2>&1
+
+timeout /t 1 > NUL 2>&1 
 
 if exist "%appdata%\EraExplorer" (
     rd /s/q "%appdata%\EraExplorer" > NUL 2>&1
@@ -18,13 +20,19 @@ if exist "%localappdata%\EraExplorer" (
 
 if exist "%temp%" (
     del /s /q  "%temp%\*.*" > NUL 2>&1
+    timeout /t 1 > NUL 2>&1
+    FOR /D %%p IN ("%temp%\*.*") DO rmdir "%%p" /s /q > NUL 2>&1
     set /a b=%a% + 1
 )
 
-
-if %b% == 0 echo Failed
+if %b% == 0 (
+    echo Failed
+    pause & exit
+)
 if %b% NEQ 0 (
     echo ERA Fixed
-    shutdown /r /t 10
+    echo Press any key to RESTART . . . 
+    pause > NUL 2>&1
+    shutdown /r /t 0
 )
-pause & exit
+
